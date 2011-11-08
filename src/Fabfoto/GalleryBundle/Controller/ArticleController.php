d2c4a5dd1504a9c5ab4273a6708886d4b805f023
fabien Garcia <garciaf@garciaf-A5sys.(none)>
2011-11-08 21:32:30 +0100

    Fix le bugArticle 	modified:   src/Fabfoto/GalleryBundle/Controller/ArticleController.php

--- src/Fabfoto/GalleryBundle/Controller/ArticleController.php
+++ src/Fabfoto/GalleryBundle/Controller/ArticleController.php
@@ -7,7 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Fabfoto\GalleryBundle\Entity\Article;
-use Fabfoto\GalleryBundle\Form\ArticleType;
+use Fabfoto\GalleryBundle\Form\Type\ArticleType;
 
 /**
  * Article controller.
