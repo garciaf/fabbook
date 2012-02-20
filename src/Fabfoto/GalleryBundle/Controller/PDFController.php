16d13b06eb72cd050b7aaba176be7b6f99ffc9bc
Garcia <fab0670312047@gmail.com>
2012-02-20 08:16:29 +0100

    On branch pdf    (use "git reset HEAD <file>..." to unstage)

--- src/Fabfoto/GalleryBundle/Controller/PDFController.php
+++ src/Fabfoto/GalleryBundle/Controller/PDFController.php
@@ -17,7 +17,7 @@ class PDFController extends Controller
 {
     /**
      * 
-     * @Route("/{slugblog}/blogarticle", name="show_article_blog_pdf")
+     * @Route("/{slugblog}/blogarticle", defaults={"_format"="pdf"}, name="show_article_blog_pdf")
      * @Pdf()
      */
     public function showBlogArticleAction($slugblog)
@@ -34,6 +34,43 @@ class PDFController extends Controller
         return $this->render('FabfotoGalleryBundle:PDF:ShowArticleBlog.pdf.twig',
                         array(
                     'article' => $article
+                ));
+    }
+     /**
+     * @Route("about", defaults={"_format"="pdf"}, name="show_about_pdf")
+     * @Pdf()
+     */
+    public function showAboutAction()
+    {
+        
+            $author = $this
+                ->getDoctrine()
+                ->getRepository('FabfotoGalleryBundle:Author')
+                ->findOneBy(array());
+ $vcard = sprintf(          
+"BEGIN:VCARD
+VERSION:3.0
+N:%s;%s
+FN:%s %s
+ORG:
+TITLE:%s
+PHOTO;
+TEL;TYPE=WORK,VOICE:(111) 555-1212
+TEL;TYPE=HOME,VOICE:(404) 555-1212
+ADR;TYPE=WORK:;;100 Waters Edge;Baytown;LA;30314;United States of America
+LABEL;TYPE=WORK:100 Waters Edge\nBaytown, LA 30314\nUnited States of America
+ADR;TYPE=HOME:;;42 Plantation St.;Baytown;LA;30314;United States of America
+LABEL;TYPE=HOME:42 Plantation St.\nBaytown, LA 30314\nUnited States of America
+EMAIL;TYPE=PREF,INTERNET:%s
+REV:20080424T195243Z
+END:VCARD", $author->getName(), $author->getFirstName(), 
+         $author->getFirstName(), $author->getName(), 
+         $author->getTitle(),
+         $author->getMail());
+        return $this->render('FabfotoGalleryBundle:PDF:ShowAbout.pdf.twig',
+                        array(
+                            'author' => $author,
+                            'vcard'  => $vcard,
                 ));
     }
  
