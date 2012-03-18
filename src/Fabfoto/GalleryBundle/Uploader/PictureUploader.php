aadd9c754f538119723fc18d73b70e91b6043807
Garcia <fab0670312047@gmail.com>
2012-03-18 11:54:11 +0100

    Generate thumb in a better way with Imagine  On branch imagineIntegration  Changes to be committed: 	modified:   app/config/config.yml

--- src/Fabfoto/GalleryBundle/Uploader/PictureUploader.php
+++ src/Fabfoto/GalleryBundle/Uploader/PictureUploader.php
@@ -31,7 +31,6 @@ class PictureUploader
 
         $newFile = $file->move($this->directory, $fileName);
         $picture->setLocation($fileName);
-        $this->generateMiniatureJPG($newFile);
     }
 
     public function upload(Picture $picture, $randomize=false)
@@ -49,38 +48,7 @@ class PictureUploader
 
         $newFile = $file->move($this->directory, $fileName);
         $picture->setLocation($fileName);
-        $this->generateMiniatureJPG($newFile);
-    }
 
-    public function generateMiniatureJPG(File $file)
-    {
-        $ratio = 128;
-        $source = imagecreatefromjpeg($file->getPathname());
-        $size = getimagesize($file->getPathname());
-        $miniature = imagecreatetruecolor($ratio, $ratio) or die("Erreur");
-        if ($size[0] > $size[1])
-        {
-            $miniature = imagecreatetruecolor(
-                    $ratio, $ratio
-            );
-            imagecopyresampled(
-                    $miniature, $source, 0, 0, 0, 0,
-                    round(($ratio / $size[1]) * $size[0]), $ratio, $size[0],
-                    $size[1]);
-        }
-        else
-        {
-            $miniature = imagecreatetruecolor(
-                    $ratio, $ratio
-            );
-            imagecopyresampled($miniature, $source, 0, 0, 0, 0, $ratio,
-                    round($size[1] * ($ratio / $size[0])), $size[0], $size[1]);
-        }
-        $miniatureName = (string) 'mini' . $file->getFilename();
-
-        $miniature = imagejpeg($miniature,
-                $file->getPath() . '/' . $miniatureName, 100);
-        return $miniature;
     }
 
     private function generateFilename(File $file, $randomize=false)
