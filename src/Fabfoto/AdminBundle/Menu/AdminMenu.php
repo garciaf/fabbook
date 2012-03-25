02ebf8994d171fc901b3e56d35f7113f7e5bd3f2
Garcia <fab0670312047@gmail.com>
2012-03-25 20:01:09 +0200

    Setup menu and some form  On branch AdminGeneratorPrefix

---
+++ src/Fabfoto/AdminBundle/Menu/AdminMenu.php
@@ -0,0 +1,48 @@
+<?php
+namespace Fabfoto\AdminBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+class AdminMenu
+{
+    private $factory;
+
+    /**
+     * @param \Knp\Menu\FactoryInterface $factory
+     */
+    public function __construct(FactoryInterface $factory)
+    {
+        $this->factory = $factory;
+    }
+    
+    /**
+     * @param Request $request
+     * @param Router $router
+     */
+    public function createAdminMenu(Request $request)
+    {
+        $menu = $this->factory->createItem('root');
+
+        $menu->setCurrentUri($request->getRequestUri());
+        
+        $menu->setchildrenAttributes(array('id' => 'main_navigation', 'class'=>'menu'));
+
+        //Media part
+        $media = $menu->addChild('Media', array('uri' => '#'));
+        $media->setLinkAttributes(array('class'=>'sub main'));
+        $media->addChild('Album', array('route' => 'Fabfoto_AdminBundle_Album_list'));
+        $media->addChild('Picture', array('route' => 'Fabfoto_AdminBundle_Picture_list'));
+        
+        //Blog Part
+        $blog = $menu->addChild('Blog', array('uri' => '#'));
+        $blog->setLinkAttributes(array('class'=>'sub main'));
+        $blog->addChild('News', array('route' => 'Fabfoto_AdminBundle_News_list'));
+        $blog->addChild('Blog', array('route' => 'Fabfoto_AdminBundle_Blog_list'));
+        $blog->addChild('Tag', array('route' => 'Fabfoto_AdminBundle_Tag_list'));
+        $blog->addChild('Messages', array('route' => 'Fabfoto_AdminBundle_Message_list'));
+        
+        return $menu;
+    }
+}
+
