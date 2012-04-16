109811cb9645d47d1ab3a1f0834c33b65e473fc4
lubuntu <administrateur@administrateur-virtual-machine.(none)>
2012-04-16 18:21:43 +0200

    + Add author

--- src/Fabfoto/GalleryBundle/Entity/Comment.php
+++ src/Fabfoto/GalleryBundle/Entity/Comment.php
@@ -5,7 +5,8 @@ namespace Fabfoto\GalleryBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use FOS\CommentBundle\Entity\Comment as BaseComment;
-
+use FOS\CommentBundle\Model\SignedCommentInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 /**
  * @ORM\Entity
  * @ORM\ChangeTrackingPolicy("DEFERRED_EXPLICIT")
@@ -36,5 +37,30 @@ class Comment extends BaseComment
     {
         return $this->id;
     }
+    /**
+     * Author of the comment
+     *
+     * @ORM\ManyToOne(targetEntity="Fabfoto\UserBundle\Entity\User")
+     * @var User
+     */
+    protected $author;
 
+    public function setAuthor(UserInterface $author)
+    {
+        $this->author = $author;
+    }
+
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    public function getAuthorName()
+    {
+        if (null === $this->getAuthor()) {
+            return 'Anonymous';
+        }
+
+        return $this->getAuthor()->getUsername();
+    }
 }
