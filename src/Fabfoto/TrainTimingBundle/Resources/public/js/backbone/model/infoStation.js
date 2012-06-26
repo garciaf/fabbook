01e8bd7ce5d585a873efd170ccb5f42b5a349ae4
lubuntu <administrateur@administrateur-virtual-machine.(none)>
2012-06-26 14:47:18 +0200

    + Delete useless files + Add models

--- src/Fabfoto/TrainTimingBundle/Resources/public/js/backbone/model/infoStation.js
+++ src/Fabfoto/TrainTimingBundle/Resources/public/js/backbone/model/infoStation.js
@@ -2,51 +2,81 @@
   // ----------
 
   // Our basic **Station** model has `name`, `x`, 'y', `done` attributes.
-  var Station = Backbone.Model.extend({
+  var InfoStation = Backbone.Model.extend({
     name: function(){},
     code: function(){},
     x: function(){},
     y: function(){},
-    
     // Default attributes for the todo item.
     defaults: function() {
       return {
-        name: "station",
-        code: "AAA",
-        x: 0,
-	y: 0
+        ligne: "station",
+        origdest: "AAA",
+        num: 0,
+	type: 0,
+        picto: 0,
+        attribut_voie: "",
+        voie: 1,
+        etat: 'OK',
+        retard: 0,
+        estimatedTime: "12"
       };
     },
-
     // Ensure that each todo created has `title`.
     initialize: function() {
      
     },
-
+    
     // Remove this Todo from *localStorage* and delete its view.
     clear: function() {
       this.destroy();
     }
 
   });
+var PropertiesInfoStation = Backbone.Model.extend({
+    initialize: function() {
+        var me = this; 
+        me.codeStation('NTS');
+    },
+    codeStation: function(code){
+        if(code){
+            return this.set('codeStation', code);
+        }else{
+            return this.get('codeStation');
+        }
+    }, 
+    url : function(){
+        var me = this; 
+        return Routing.generate('liste_timing_station', {"codeGare" : me.codeStation()});
+    }
+});
+
+var properties = new PropertiesInfoStation();
 
 // Station Collection
   // ---------------
 
   // The collection of todos is backed by *localStorage* instead of a remote
   // server.
-  var StationList = Backbone.Collection.extend({
-
+  var InfoStationList = Backbone.Collection.extend({
+      
     // Reference to this collection's model.
-    model: Station,
-    // 
-    url : Routing.generate('liste_gare'),
+    model: InfoStation,
+    codeStation: function(code){
+        return properties.codeStation(code);
+    }, 
+    url : function(){
+        return properties.url();
+    },
+    clearList: function(){
+        this.remove(this.models);
+    },
     initialize: function(options) {
         var me = this; 
-        $.getJSON(me.url, function(data) {
-        
+        $.getJSON(me.url(), function(data) {
+        console.log(data);
         $.each(data, function(idx, stationJSON) {
-        station = new Station();
+        station = new InfoStation();
         station.set('name', stationJSON.name);
         station.set('code', stationJSON.code_ddg);
         station.set('x', stationJSON.x);
@@ -54,29 +84,18 @@
         me.add(station);
         
 //        console.log(x);
-        });
+        
         
         });
-    },
+    });
+    }
+    ,
     fetch: function(callBack){$.getJSON(this.url, callBack);
     },
-    updateMap: function(map){
+    refresh: function(){
         this.fetch(function(data){
-            console.log(data.y);
-            $.each(data, function(idx, stationJSON) {
-            map.addMarker({
-            lat: stationJSON.x,
-            lng: stationJSON.y,
-            title: stationJSON.name,
-            click: function(e) {
-                InfoStations.codeStation(stationJSON.code_ddg);
-                console.log('stationList changed to '+InfoStations.codeStation());
-            }
-            });
-            });
+            
         });
-        //map.createMarker();
-        //console.log(map);
     },
     // Save all of the todo items under the `"todos"` namespace.
     //localStorage: new Store("stations-backbone"),
@@ -100,6 +119,6 @@
   });
   
   // Create our global collection of **Todos**.
-  var Stations = new StationList();
+  var InfoStations = new InfoStationList();
 
   
