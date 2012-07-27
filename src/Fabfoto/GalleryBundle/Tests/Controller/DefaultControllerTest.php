f33766f25ab0decd2e2a58187bec0da0a6776f54
Garcia fabien <garciaf@garciaf-N140.(none)>
2012-07-27 20:36:59 +0200

    Delete auto generated test class and add real functionnel test

--- src/Fabfoto/GalleryBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Fabfoto/GalleryBundle/Tests/Controller/DefaultControllerTest.php
@@ -12,6 +12,8 @@ class DefaultControllerTest extends WebTestCase
 
         $crawler = $client->request('GET', '/news');
 
-        $this->assertTrue($crawler->filter('html:contains("nouvelles")')->count() >= 2);
+        $this->assertTrue($crawler->filter('html:contains("nouvelles")')->count() > 0);
+        $this->assertTrue($crawler->filter('html:contains("articles")')->count() > 0);
+        $this->assertTrue($crawler->filter('html:contains("fabbook")')->count() > 0);
     }
 }
