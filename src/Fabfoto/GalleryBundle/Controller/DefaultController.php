61583b89f9b40ebc9a9b1810b7a580c6354873a0
Garcia fabien <garciaf@garciaf-N140.(none)>
2012-09-06 09:19:28 +0200

    Add ESIL support for cache and remove cache for notification

--- src/Fabfoto/GalleryBundle/Controller/DefaultController.php
+++ src/Fabfoto/GalleryBundle/Controller/DefaultController.php
@@ -7,20 +7,24 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Component\HttpFoundation\Request;
-use Fabfoto\GalleryBundle\Entity\Album  as Album;
-use Fabfoto\GalleryBundle\Entity\Tag  as Tag;
-use Fabfoto\GalleryBundle\Entity\ArticleBlog  as ArticleBlog;
+use Fabfoto\GalleryBundle\Entity\expiresAlbum as Album;
+use Fabfoto\GalleryBundle\Entity\Tag as Tag;
+use Fabfoto\GalleryBundle\Entity\ArticleBlog as ArticleBlog;
 use Fabfoto\GalleryBundle\Entity\Category as Category;
 
-class DefaultController extends BaseController
-{
+class DefaultController extends BaseController {
+    /**
+     * @Cache(public=false, maxage= 1)
+     */
+    public function notificationAction() {
+        return $this->render('FabfotoGalleryBundle::notification.html.twig');
+    }
 
     /**
      * @Cache(expires="tomorrow")
      * @Route("/news", name="show_articles")
      */
-    public function showHomePageAction()
-    {
+    public function showHomePageAction() {
         $articlesQuery = $this->getNewsQuery();
         $articlesBlog = $this->getBlogs($this->container->getParameter('nbArticle'));
         $albums = $this->getAlbums($this->container->getParameter('nbAlbum'));
@@ -36,8 +40,7 @@ class DefaultController extends BaseController
      * @Cache(expires="tomorrow")
      * @Route("blog", name="index_blog")
      */
-    public function indexBlogsAction()
-    {
+    public function indexBlogsAction() {
         $articlesBlogsQuery = $this->getBlogsQuery();
 
         $articlesBlog = $this->getBlogs($this->container->getParameter('nbArticle'));
@@ -53,8 +56,7 @@ class DefaultController extends BaseController
      * @Route("/{slugblog}/blogarticle", name="show_article_blog")
      * @ParamConverter("article", class="FabfotoGalleryBundle:ArticleBlog")
      */
-    public function showBlogArticleAction(ArticleBlog $article)
-    {
+    public function showBlogArticleAction(ArticleBlog $article) {
         //If the artcle is not published you can not access to it
         if (!$article->getIsPublished()) {
             $this->createNotFoundException();
@@ -70,8 +72,7 @@ class DefaultController extends BaseController
      * @Route("/{slug}/tag/blogarticle", name="show_articles_blog_by_tags")
      * @ParamConverter("tag", class="FabfotoGalleryBundle:Tag")
      */
-    public function showBlogArticleByTagAction(Tag $tag)
-    {
+    public function showBlogArticleByTagAction(Tag $tag) {
         $articlesBlogs = $this->getBlogs(null, $tag);
 
         return $this->render('FabfotoGalleryBundle:Default:IndexTagArticleBlog.html.twig', array(
@@ -84,11 +85,10 @@ class DefaultController extends BaseController
      * @Cache(expires="+1 hours")
      * @Route("albums", name="index_album")
      */
-    public function indexAlbumsAction()
-    {
-            $albumsQuery = $this->getAlbumsQuery();
+    public function indexAlbumsAction() {
+        $albumsQuery = $this->getAlbumsQuery();
 
-            return $this->render('FabfotoGalleryBundle:Default:indexAlbum.html.twig', array('albums' => $this->getPager($albumsQuery)));
+        return $this->render('FabfotoGalleryBundle:Default:indexAlbum.html.twig', array('albums' => $this->getPager($albumsQuery)));
     }
 
     /**
@@ -98,8 +98,7 @@ class DefaultController extends BaseController
      * @Route("{slug}/album", name="show_album")
      * @ParamConverter("album", class="FabfotoGalleryBundle:Album")
      */
-    public function showAlbumAction(Album $album)
-    {
+    public function showAlbumAction(Album $album) {
         $pictures = $this->getAlbumPicture($album, false, true);
 
         $backgrounds = $this->getAlbumPicture($album, true);
@@ -108,24 +107,21 @@ class DefaultController extends BaseController
 
         $category = $album->getCategory();
         if ($category) {
-            if ($category->getSlug() == $this->container->getParameter('slugNoteCategory') ) {
+            if ($category->getSlug() == $this->container->getParameter('slugNoteCategory')) {
                 $template = "FabfotoGalleryBundle:Default:ShowNoteBook.html.twig";
-            } elseif ($category->getSlug() == $this->container->getParameter('slugAlbumCategory') ) {
+            } elseif ($category->getSlug() == $this->container->getParameter('slugAlbumCategory')) {
                 $template = "FabfotoGalleryBundle:Default:ShowAlbum.html.twig";
             }
-
         }
 
-        return $this->render($template,
-                array(
+        return $this->render($template, array(
                     'pictures' => $pictures,
                     'album' => $album,
                     'backgrounds' => $backgrounds
                 ));
     }
 
-    public function allBackgroundAction($max)
-    {
+    public function allBackgroundAction($max) {
         $backgrounds = $this->getAlbumPicture(null, true, false);
         shuffle($backgrounds);
 
@@ -137,8 +133,7 @@ class DefaultController extends BaseController
     /**
      * @Route("search", name="fabfoto_search")
      */
-    public function searchAction(Request $request)
-    {
+    public function searchAction(Request $request) {
         $pictures = array();
         $albums = array();
         if ($request->query->get('q')) {
