5367aa2e42a9604b03fd2f69806615e4de9b68eb
Fabien Garcia <fab0670312047@gmail.com>
2012-10-07 21:52:22 +0200

    Add tcpdf support

--- src/Fabfoto/GalleryBundle/Controller/PDFController.php
+++ src/Fabfoto/GalleryBundle/Controller/PDFController.php
@@ -4,8 +4,10 @@ namespace Fabfoto\GalleryBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Fabfoto\UserBundle\Entity\User as User;
+use Fabfoto\GalleryBundle\Entity\ArticleBlog  as ArticleBlog;
 use Ps\PdfBundle\Annotation\Pdf;
 /**
  * Article controller.
@@ -14,23 +16,46 @@ use Ps\PdfBundle\Annotation\Pdf;
  */
 class PDFController extends BaseController
 {
+    /**
+     * @Cache(expires="+1 week", public=true)
+     * @Route("/{slugblog}/blog", defaults={"_format"="pdf"}, name="show_article_blog_pdf")
+     * @ParamConverter("article", class="FabfotoGalleryBundle:ArticleBlog")
+     */
+    public function showBlogArticleAction(ArticleBlog $article)
+    {
+        $format = $this->get('request')->get('_format');
+        $pdfObj = $this->get("white_october.tcpdf")->create();
+
+        $html = $this->renderView('FabfotoGalleryBundle:PDF:ShowArticleBlog.pdf.twig', array(
+            'article' => $article
+                ));
+        $pdfObj->AddPage();
+        $pdfObj->writeHTML($html, true, false, true, false, '');
+        $pdfObj->lastPage();
+
+        return $pdfObj->Output($article->getSlugblog() . '.pdf');
+    }
 
     /**
-     * @Pdf()
-     * @Route("/{slug}/pdfcard", name="show_about_pdf_from", defaults={"_format"="pdf"})
+     * @Route("/{slug}/card", name="show_about_pdf_from", defaults={"_format"="pdf"})
      * @ParamConverter("user", class="FabfotoUserBundle:User")
      */
     public function showPDFCardAction(User $user)
     {
+        $pdfObj = $this->get("white_october.tcpdf")->create();
         $vcard= $this->getVcardOfUser($user);
-
-        return $this->render('FabfotoGalleryBundle:PDF:ShowAbout.pdf.twig', array(
+        $html = $this->renderView('FabfotoGalleryBundle:PDF:ShowAbout.pdf.twig', array(
             'user' => $user,
             'vcard' =>$vcard
                 ));
+        $pdfObj->SetFont('dejavusans', '', 6);
+        $pdfObj->setPrintHeader(false);
+        $pdfObj->setPrintFooter(false);
+        $pdfObj->AddPage('P', 'BUSINESS_CARD_FR');
+        $pdfObj->writeHTML($html, true, false, true, false, '');
+        $pdfObj->lastPage();
 
-//        return $this->render($html);
-//        return $pdfObj->Output($user->getSlug().'carte.pdf');
+        return $pdfObj->Output($user->getSlug().'carte.pdf');
     }
 
 }
